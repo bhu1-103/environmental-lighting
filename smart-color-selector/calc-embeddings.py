c00159db3ccdae279458688a69a1c73660cafaa8
bhu1-103 <103.bhuvan@gmail.com>
2026-05-28 00:28:21 +0530

    storing in numpy array instead of json files

--- smart-color-selector/calc-embeddings.py
+++ smart-color-selector/calc-embeddings.py
@@ -1,50 +1,33 @@
 import json
 import ollama
 from datetime import datetime
-from rich import print
 from rich.progress import track
-
-print("loading colors...")
+import numpy as np
 
 with open("colornames.json") as f:
     colors = json.load(f)
 
-embedded_colors = []
-total = len(colors)
-start_time = datetime.now()
+metadata = []
+embeddings = []
 
-for i,color in enumerate(track(colors, description="Embedding colors...", transient=False), start=1):
+for i, color in enumerate(track(colors, description="Embedding colors...", transient=False)):
     response = ollama.embeddings(
         model='all-minilm',
         prompt=color["name"]
     )
 
-    embedded_colors.append({
+    metadata.append({
         "name": color["name"],
         "hex": color["hex"],
-        "embedding": response["embedding"]
     })
-    
-    '''if i % 300 == 0:
 
-        percentage = i/total*100
-        now = datetime.now()
-        difference = now - start_time
-        seconds_elapsed = difference.total_seconds()
-        time_per_embedding = (seconds_elapsed / i)
-        ER_minutes  = int(time_per_embedding * total) // 60
-        ER_seconds  = (time_per_embedding * total) % 60
-        ETA_minutes = (time_per_embedding * (total - i)) // 60
-        ETA_seconds = (time_per_embedding * (total - i)) % 60
-        print("------------------------------------------")
-        print(f"[#ffffff]{i}/{total}[/#ffffff] embeddings done. [#ffffff]{percentage:.2f}%[/#ffffff] complete")
-        print(f"Expected runtime: [#ff0066]{ER_minutes}[/#ff0066] minutes [#ff0066]{ER_seconds:.0f}[/#ff0066] seconds")
-        print(f"ETA: [#00ffff]{ETA_minutes:.0f}[/#00ffff] minutes [#00ffff]{ETA_seconds:.0f}[/#00ffff] seconds")
-        print("------------------------------------------")'''
+    embeddings.append(response["embedding"])
 
-print("Saving to file db.json")
+print("Saving files")
 
-with open("db.json","w") as f:
-    json.dump(embedded_colors, f)
+with open("metadata.json", "w") as f:
+    json.dump(metadata,f)
 
-    print("Done")
+np.save("embeddings.npy", np.array(embeddings, dtype=np.float32))
+
+print("Done")
